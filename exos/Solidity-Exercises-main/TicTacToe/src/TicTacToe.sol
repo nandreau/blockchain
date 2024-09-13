339b33da29b57d88ab16d64cc35b54fc4e1206df
Aurélien <aurelienpenotperbet@gmail.com>
2024-09-13 12:48:13 +0200

    feat: exercice tictactoe

--- exos/Solidity-Exercises-main/TicTacToe/src/TicTacToe.sol
+++ exos/Solidity-Exercises-main/TicTacToe/src/TicTacToe.sol
@@ -11,6 +11,24 @@ contract TicTacToe {
     */
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
+        for (uint256 i=0; i<3; i++) {
+            if (board[i][0] == 1 && board[i][1] == 1 && board[i][2] ==  1 || board[i][0] == 0 && board[i][1] == 0 && board[i][2] ==  0){
+                return true;
+            }
+        }
+
+        for (uint256 i=0; i<3; i++) {
+            if (board[0][i] == 1 && board[1][i] == 1 && board[2][i] ==  1 || board[0][i] == 0 && board[1][i] == 0 && board[2][i] ==  0){
+                return true;
+            }
+        }
+
+        for (uint256 i=0; i<2; i++) {
+            if (board[0][0] == i && board[1][1] == i && board[2][2] ==  i || board[2][0] == i && board[1][1] == i && board[0][2] ==  i){
+                return true;
+            }
+        }
+
+        return false;
     }
 }
